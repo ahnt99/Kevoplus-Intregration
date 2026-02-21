20f6b2cc57e196e159380ef564bd4303a61222b7
ahnt99 <112784531+ahnt99@users.noreply.github.com>
2026-02-20 23:03:51 -0500

    Refactor config flow for Kevo integration

--- config_flow.py
+++ config_flow.py
@@ -1,21 +1,43 @@
 import voluptuous as vol
-from homeassistant import config_entries
-from .const import DOMAIN
 
-class MyIntegrationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
+from homeassistant import config_entries
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+
+from .const import DOMAIN
+from .api import KevoApi, KevoAuthError
+
+
+class KevoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
+
         errors = {}
 
         if user_input is not None:
-            return self.async_create_entry(
-                title="My Integration",
-                data={"api_key": user_input["api_key"]},
-            )
+            api = KevoApi()
 
-        data_schema = vol.Schema({
-            vol.Required("api_key"): str,
-        })
+            try:
+                await api.login(
+                    user_input[CONF_USERNAME],
+                    user_input[CONF_PASSWORD],
+                )
+            except KevoAuthError:
+                errors["base"] = "invalid_auth"
+            except Exception:
+                errors["base"] = "cannot_connect"
+            else:
+                return self.async_create_entry(
+                    title="Kevo Plus",
+                    data=user_input,
+                )
 
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                }
+            ),
+            errors=errors,
+        )
